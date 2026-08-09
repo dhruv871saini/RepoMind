a7eb04ea636d20e426148dc7029fe47793650cf1
Dhruv Saini <dhruvsaini871@gmail.com>
2026-08-09 19:17:15 +0530

    Update model settings in setting.py
    Updated OLLAMA_EMBED_MODEL and OLLAMA_CHAT_MODEL for improved model selection.

--- backend/app/setting.py
+++ backend/app/setting.py
@@ -13,6 +13,10 @@ class Settings(BaseSettings):
     OLLAMA_EMBED_MODEL: str = "nomic-embed-text"
     OLLAMA_CHAT_MODEL: str = "llama3.2"
 
+    #add this for better 
+    OLLAMA_EMBED_MODEL: str = "qwen3-embedding:4b"
+    OLLAMA_CHAT_MODEL: str = "cieloforge/qwen2.5-14B-instruct-spec"
+
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
